bec6df843da1fd0b104bf8ac75cb4990f7bfa85c
ahmedoothman <ahmed.othman.ghazzawi@gmail.com>
2024-07-28 18:48:25 +0300

    edited data

--- utils/generateData.js
+++ utils/generateData.js
@@ -19,7 +19,7 @@ async function generateRandomData() {
     const tempPassword = faker.internet.password();
     for (let i = 0; i < 10; i++) {
         const user = new User({
-            name: faker.internet.userName(),
+            name: faker.person.fullName(),
             email: faker.internet.email(),
             password: tempPassword,
             passwordConfirm: tempPassword,
@@ -38,22 +38,50 @@ async function generateRandomData() {
     //read all realtors from the users array
     for (let i = 0; i < 100; i++) {
         const realtors = users.filter((user) => user.role === 'realtor');
+        // at least two bedrooms
+        const bedrooms = Math.floor(Math.random() * 4) + 2;
+        const type = houseTypes[Math.floor(Math.random() * houseTypes.length)];
         const house = new House({
             title: faker.lorem.words(),
             description: faker.lorem.paragraph(),
-            price: faker.commerce.price(),
-            type: houseTypes[Math.floor(Math.random() * houseTypes.length)],
+            price:
+                type === 'rent'
+                    ? faker.commerce.price({
+                          min: 1000,
+                          max: 5000,
+                      })
+                    : faker.commerce.price({
+                          min: 600000,
+                          max: 1500000,
+                      }),
+            type: type,
             realtor: realtors[Math.floor(Math.random() * realtors.length)]._id,
-            imageUrl: faker.image.imageUrl(600, 500, 'realtor'),
+            imageUrl: faker.image.urlLoremFlickr({
+                width: 600,
+                height: 500,
+                category: 'realtor',
+            }),
             images: [
-                faker.image.imageUrl(600, 500, 'realtor'),
-                faker.image.imageUrl(600, 500, 'realtor'),
-                faker.image.imageUrl(600, 500, 'realtor'),
+                faker.image.urlLoremFlickr({
+                    width: 600,
+                    height: 500,
+                    category: 'realtor',
+                }),
+                faker.image.urlLoremFlickr({
+                    width: 600,
+                    height: 500,
+                    category: 'realtor',
+                }),
+                faker.image.urlLoremFlickr({
+                    width: 600,
+                    height: 500,
+                    category: 'realtor',
+                }),
             ],
             rate: Math.floor(Math.random() * 6),
-            bedrooms: Math.floor(Math.random() * 6),
-            bathrooms: Math.floor(Math.random() * 6),
-            area: Math.floor(Math.random() * 500),
+            bedrooms: bedrooms,
+            bathrooms: Math.floor(Math.random() * bedrooms) + 1,
+            area: Math.floor(Math.random() * 500) + 90,
             address: {
                 street: faker.location.streetAddress(),
                 city: faker.location.city(),
